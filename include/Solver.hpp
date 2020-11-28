2c5ecd0ba9f1c3c57e8efcae7e9827529f44356c
Jayson Sho Toma <m.kagamiz@gmail.com>
2020-11-28 16:32:58 +0900

    modify solver template

--- include/Solver.hpp
+++ include/Solver.hpp
@@ -15,31 +15,49 @@ namespace SPOPT {
             double primalTolerance;
             double dualTolerance;
             double gapTolerance;
+            int reportFrequency;
     };
 
     class Solver {
         public:
-            // constructors
-            Solver(){};
-            // Reads solver settings from the file `filename` and store it on `solverConfig`
-            // when instanciating the Solver.
-            // Internally, it calls a function LoadConfig.
-            Solver(std::string fileName);
-
             // Reads solver settings from the file `filename` and store it on `solverConfig`.
             // TODO: error handling
             virtual void LoadConfig(std::string fileName);
 
             void Solve(const ProblemData &problemData);
             void EnumerateStationaryPoints(const ProblemData &problemData);
-        private:
+        
+        protected:
+            bool IsTerminationCriterionSatisfied(const ProblemData &problemData, const Eigen::VectorXd &v);
 
+            virtual void SetUpFrom(const ProblemData &problemData) = 0;
             virtual Eigen::VectorXd ConstructInitialPoint(const ProblemData &problemData) = 0;
-            virtual bool IsTerminationCriterionSatisfied(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
             virtual Eigen::VectorXd ApplyFixedPointFunction(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
+            virtual void UpdateParameter(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
+
+            virtual double GetPrimalInfeasibility(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
+            virtual double GetDualInfeasibility(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
+            virtual double GetPrimalObjValue(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
+            virtual double GetDualObjValue(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
+            virtual double GetGap(const ProblemData &problemData, const Eigen::VectorXd &v) = 0;
+
+            inline const Eigen::SparseMatrix<double> &MatrixA(const ProblemData &problemData) { return problemData.A; }
+
+            inline const Eigen::VectorXd &VectorB(const ProblemData &problemData) { return problemData.b; }
+            inline const double originalBNorm(const ProblemData &problemData) { return problemData.originalBNorm; }
+
+            inline const Eigen::SparseVector<double> &VectorC(const ProblemData &problemData) { return problemData.c; }
+            inline const double originalCNorm(const ProblemData &problemData) { return problemData.originalCNorm; }
+            
+            inline const std::vector<int> &psdMatrixSizes(const ProblemData &problemData) { return problemData.psdMatrixSizes; }
+
+            inline const double primalScaler(const ProblemData &problemData) { return problemData.primalScaler; }
+            inline const double dualScaler(const ProblemData &problemData) { return problemData.dualScaler; }
+            inline const Eigen::VectorXd &VectorD(const ProblemData &problemData) { return problemData.D; }
+            inline const Eigen::VectorXd &VectorE(const ProblemData &problemData) { return problemData.E; }
 
             YAML::Node solverConfig;
-            Parameter param;
+            Parameter basicParam;
     };
 }
 #endif //__SOLVER_HPP__
--- src/Solver.cpp
+++ src/Solver.cpp
@@ -2,29 +2,50 @@
 
 namespace SPOPT {
 
-    Solver::Solver(std::string fileName)
-    {
-        LoadConfig(fileName);
-    }
-
     void Solver::LoadConfig(std::string fileName)
     {
         solverConfig = YAML::LoadFile(fileName);
-
-        param.enableAndersonAcceleration = solverConfig["enableAndersonAcceleration"].as<bool>(false);
-        param.primalTolerance            = solverConfig["primalTolerance"].as<double>(1e-3);
-        param.dualTolerance              = solverConfig["dualTolerance"].as<double>(1e-3);
-        param.gapTolerance               = solverConfig["gapTolerance"].as<double>(1e-3);
+        basicParam.enableAndersonAcceleration = solverConfig["enableAndersonAcceleration"].as<bool>(false);
+        basicParam.primalTolerance            = solverConfig["primalTolerance"].as<double>(1e-3);
+        basicParam.dualTolerance              = solverConfig["dualTolerance"].as<double>(1e-3);
+        basicParam.gapTolerance               = solverConfig["gapTolerance"].as<double>(1e-3);
+        basicParam.reportFrequency            = solverConfig["reportFrequency"].as<int>(1);
     }
 
     void Solver::Solve(const ProblemData &problemData)
     {
+        SetUpFrom(problemData);
         int iterationCounter = 0;
         Eigen::VectorXd v_i = ConstructInitialPoint(problemData);
 
+        if (basicParam.reportFrequency > 0) {
+            std::cout << "Ite No.  PFEAS   DFEAS    GAP         PVAL            DVAL     " << std::endl;
+            std::cout << "===============================================================" << std::endl;
+        }
+
         while (!IsTerminationCriterionSatisfied(problemData, v_i)) {
+            if (basicParam.reportFrequency > 0 && iterationCounter % basicParam.reportFrequency == 0) {
+                std::cout << std::setw(7)  << iterationCounter << " "
+                          << std::setw(7)  << std::scientific << GetPrimalInfeasibility(problemData, v_i) << " "
+                          << std::setw(7)  << std::scientific << GetDualInfeasibility(problemData, v_i)   << " "
+                          << std::setw(7)  << std::scientific << GetGap(problemData, v_i)                 << " "
+                          << std::setw(15) << std::scientific << GetPrimalObjValue(problemData, v_i)      << " "
+                          << std::setw(15) << std::scientific << GetDualObjValue(problemData, v_i)
+                          << std::endl;
+                std::cout << std::resetiosflags;
+            }
             iterationCounter++;
             v_i = ApplyFixedPointFunction(problemData, v_i);
+            UpdateParameter(problemData, v_i);
         }
+    }
+
+    bool Solver::IsTerminationCriterionSatisfied(const ProblemData &problemData, const Eigen::VectorXd &v)
+    {
+        double primalInfeasibility = GetPrimalInfeasibility(problemData, v);
+        double dualInfeasibility   = GetDualInfeasibility(problemData, v);
+        double gap                 = GetGap(problemData, v);
+
+        return primalInfeasibility <= basicParam.primalTolerance && dualInfeasibility <= basicParam.dualTolerance && gap <= basicParam.gapTolerance;
     }
 }
